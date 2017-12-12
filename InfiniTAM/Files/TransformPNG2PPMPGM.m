02eed5058dd65c12b047d976df1bd24adf88fa09
HaoguangHuang <734194033@qq.com>
2017-12-12 20:12:22 +0800

    backup

--- InfiniTAM/Files/TransformPNG2PPMPGM.m
+++ InfiniTAM/Files/TransformPNG2PPMPGM.m
@@ -1,23 +1,23 @@
 function TransformPNG2PPMPGM()
     depthSet_png = {};
     rgbSet_png = {};
-    total = 19;
+    total = 200;
 
-    depthFile_pgm_address = '/home/hhg/Documents/myGithub2/InfiniTAM_v2_hhg/InfiniTAM/Files/wajueji/Frames2/';
-    rgbFile_ppm_address = '/home/hhg/Documents/myGithub2/InfiniTAM_v2_hhg/InfiniTAM/Files/wajueji/Frames2/';
+    depthFile_pgm_address = '/home/hhg/Documents/myGithub2/tool/oni2picture_ed2/tankData/MATLAB/node_seg/output/imageSource/';
+    rgbFile_ppm_address = '/home/hhg/Documents/myGithub2/tool/oni2picture_ed2/tankData/MATLAB/node_seg/output/imageSource/';
     
-    png_address = '/home/hhg/Documents/myGithub2/InfiniTAM_v2_hhg/InfiniTAM/Files/wajueji/old2/';
+    png_address = '/home/hhg/Documents/myGithub2/tool/oni2picture_ed2/tankData/MATLAB/node_seg/input/Wajueji_2/extractdata_afterDRev/';
     %%process
     for i = 1:total
         %%read png image
-        depthFileName_png = [png_address,'d_',int2str(i+181),'.png'];
-        rgbFileName_png = [png_address,'c_',int2str(i+181),'.png'];
+        depthFileName_png = [png_address,'d_',int2str(i),'.png'];
+        rgbFileName_png = [png_address,'c_',int2str(i),'.png'];
         depthSet_png(i).data = imread(depthFileName_png);
         rgbSet_png(i).data = imread(rgbFileName_png);
         
         %%transform png into pgm(depth) and ppm(color)
-        imwrite(depthSet_png(i).data,[depthFile_pgm_address,int2str(i-1),'.pgm']);
-        imwrite(rgbSet_png(i).data,[rgbFile_ppm_address,int2str(i-1),'.ppm']);
+        imwrite(depthSet_png(i).data,[depthFile_pgm_address,int2str(i),'.pgm']);
+        imwrite(rgbSet_png(i).data,[rgbFile_ppm_address,int2str(i),'.ppm']);
     end
     
 end
